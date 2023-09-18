468e68bd4ac56ff663b9668b21856c5b458c1147
paularays <144204494+paularays@users.noreply.github.com>
2023-09-17 22:40:32 -0600

    Add files via upload

---
+++ tarea 4/programa 3 tarea 4.py
@@ -0,0 +1,14 @@
+#programa 3
+
+coordenadaX=float(input("coordenada x"))
+coordenadaY=float(input("coordenada y"))
+
+x=(coordenadaX - 2.5) // 15
+y=(coordenadaY - 2.5) // 15
+
+n=(y * 8) + x
+
+
+print("el numero de tornillo es:",n)
+
+print("fin")
